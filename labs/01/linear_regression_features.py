0e773688c904e602bb99a1eabf72d9783693e740
František Šuráň <frantiseksuran@Frantiseks-Laptop.local>
2024-10-10 17:28:33 +0200

    pavel

--- labs/01/linear_regression_features.py
+++ labs/01/linear_regression_features.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 
+import pandas as pd
 import numpy as np
 import sklearn.linear_model
 import sklearn.metrics
@@ -22,25 +23,37 @@ def main(args: argparse.Namespace) -> list[float]:
     xs = np.linspace(0, 7, num=args.data_size)
     ys = np.sin(xs) + np.random.RandomState(args.seed).normal(0, 0.2, size=args.data_size)
 
+    data = pd.DataFrame(data = ys, columns = ["target"])
+    
     rmses = []
     for order in range(1, args.range + 1):
         # TODO: Create features `(x^1, x^2, ..., x^order)`, preferably in this ordering.
         # Note that you can just append `x^order` to the features from the previous iteration.
+        data[f'xs_{order}'] = xs ** order
 
         # TODO: Split the data into a train set and a test set.
         # Use `sklearn.model_selection.train_test_split` method call, passing
         # arguments `test_size=args.test_size, random_state=args.seed`.
+        data_train, data_test = sklearn.model_selection.train_test_split(data, test_size = args.test_size, random_state = args.seed)
+
+        X_train = data_train.iloc[:, 1:] 
+        target_train = data_train.iloc[:, 0]
+
+        X_test = data_test.iloc[:, 1:]
+        target_test = data_test.iloc[:, 0]
 
         # TODO: Fit a linear regression model using `sklearn.linear_model.LinearRegression`;
         # consult the documentation and see especially the `fit` method.
-        model = ...
+        fandaisgay = sklearn.linear_model.LinearRegression()
+        fandaisgay.fit(X_train, target_train)
 
         # TODO: Predict targets on the test set using the `predict` method of the trained model.
+        fanduvpindikjemalej = fandaisgay.predict(X_test)
 
         # TODO: Compute root mean square error on the test set predictions.
         # You can either do it manually, or you can look at the metrics offered
         # by the `sklearn.metrics` module.
-        rmse = ...
+        rmse = np.sqrt(sklearn.metrics.mean_squared_error(target_test, fanduvpindikjemalej))
 
         rmses.append(rmse)
 
@@ -51,8 +64,8 @@ def main(args: argparse.Namespace) -> list[float]:
             if args.plot is not True:
                 plt.gcf().get_axes() or plt.figure(figsize=(6.4*3, 4.8*3))
                 plt.subplot(3, 3, 1 + len(plt.gcf().get_axes()))
-            plt.plot(train_data[:, 0], train_target, "go")
-            plt.plot(test_data[:, 0], test_target, "ro")
+            plt.plot(np.array(X_train.iloc[:, 0]), np.array(target_train), "go")
+            plt.plot(np.array(X_test.iloc[:, 0]), np.array(target_test), "ro")
             plt.plot(np.linspace(xs[0], xs[-1], num=100),
                      model.predict(np.power.outer(np.linspace(xs[0], xs[-1], num=100), np.arange(1, order + 1))), "b")
             plt.show() if args.plot is True else plt.savefig(args.plot, transparent=True, bbox_inches="tight")
